d61090c7f8e4fdfe1c506c59760a87665ac0df97
Allen Shin <geeuho@gmail.com>
2019-12-10 12:20:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    let cooltext = document.getElementById("text")
+    cooltext.textContent = "This is really cool!"
+})
